3d871174cc80909e7126c47d8ab4f806497232bc
thisisnotdevin <devin1299@outlook.com>
2023-03-30 22:37:27 -0400

    commited changes

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendEmail() {
+    window.location.href = "mailto:devin1299@outlook.com";
+  }
+
+function scrollBy100vh() {
+    window.scrollBy({
+      top: window.innerHeight,
+      behavior: 'smooth'
+    });
+}
+  
