31fc0cec50a9220e4a81abe23a7bc062719164cf
thisisnotdevin <devin1299@outlook.com>
2023-03-31 17:31:40 -0400

    added about m

--- src/script.js
+++ src/script.js
@@ -2,13 +2,28 @@ function sendEmail() {
     window.location.href = "mailto:devin1299@outlook.com";
   }
 
-function scrollBy100vh() {
+function scrollBy200vh() {
     window.scrollBy({
-      top: window.innerHeight,
+      top: window.innerHeight * 2,
       behavior: 'smooth'
     });
 }
- 
+
+function scrollBy100vh() {
+  window.scrollBy({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
+}
+function scrollToBottom() {
+  window.scrollBy({
+    top: window.innerHeight * 5,
+    behavior: 'smooth'
+  });
+}
+function openPDF() {
+  window.open('../myres/resume-devin.pdf', '_blank');
+}
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
@@ -19,3 +34,18 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+var navbar = document.querySelector('.navbar');
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+  }
+  prevScrollpos = currentScrollPos;
+}
+
