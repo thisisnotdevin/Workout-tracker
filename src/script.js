e69c9fdb207ad7adba8c34634f93b2d91944444f
thisisnotdevin <devin1299@outlook.com>
2023-03-31 15:13:34 -0400

    remove anstro

--- src/script.js
+++ src/script.js
@@ -8,4 +8,14 @@ function scrollBy100vh() {
       behavior: 'smooth'
     });
 }
-  
+ 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
