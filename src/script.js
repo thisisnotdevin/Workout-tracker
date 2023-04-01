71353792bc977ace7995adc2d21f973a57674e50
thisisnotdevin <devin1299@outlook.com>
2023-03-31 21:17:49 -0400

    added section

--- src/script.js
+++ src/script.js
@@ -1,45 +1,44 @@
 function sendEmail() {
-    window.location.href = "mailto:devin1299@outlook.com";
-  }
+  window.location.href = "mailto:devin1299@outlook.com";
+}
 
 function scrollBy200vh() {
-    window.scrollBy({
-      top: window.innerHeight * 2,
-      behavior: 'smooth'
-    });
+  window.scrollBy({
+    top: window.innerHeight * 2,
+    behavior: "smooth",
+  });
 }
 
 function scrollBy100vh() {
   window.scrollBy({
     top: window.innerHeight,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 function scrollToBottom() {
   window.scrollBy({
     top: window.innerHeight * 5,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 function openPDF() {
   window.open("../myres/resume-devin.pdf", "_blank");
 }
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry)
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-      }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
 });
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-var navbar = document.querySelector('.navbar');
+var navbar = document.querySelector(".navbar");
 var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     navbar.classList.remove("hidden");
@@ -47,5 +46,5 @@ window.onscroll = function() {
     navbar.classList.add("hidden");
   }
   prevScrollpos = currentScrollPos;
-}
+};
 
